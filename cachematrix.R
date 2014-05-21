f539bebd5e1bb68e9299f6d041f9d43811d846b3
Radcliffe101 <atarrant@whoi.edu>
2014-05-21 13:26:11 -0400

    Filled in functions for making and retrieving cache matrices
    See detailed comments within R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special matrix object that can cache its inverse.
+## It uses the << operator to assign the matrix to a different environment.
 
-## Write a short comment describing this function
+## This function accepts a matrix, x, as its sole arguement. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function (y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get, 
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## This function, which drove me crazy, will calculate the inverse of a matrix using the solve 
+## function. First it checks to see whether the inverse has already been calculated. If so
+## it retrieves it from the cache. Otherwise it will calculate it. 
 cacheSolve <- function(x, ...) {
+        m <- x$getsolve()
+         if (!is.null(m)) {
+                 message("getting cached data")
+                 return(m)
+         }
+         data <- x$get()
+         m <- solve(data, ...)
+         x$setsolve(m)
+         m
         ## Return a matrix that is the inverse of 'x'
 }
+
+## To test this you could first define a matrix. For example:
+## z <- matrix(c(1,1,1,3,4,3,3,3,4), nrow=3, ncol=3)
+## a <- makeCacheMatrix()
+## a$set(matrix(z))
+## a$get
+## cachesolve(a)
